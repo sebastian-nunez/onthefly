ba96c3df9ef3708cf1794b1cbc4ce2a68a3aa8dd
sebastian-nunez <02snunez@gmail.com>
2023-10-14 15:31:59 -0400

    added activities controller

---
+++ server/controllers/activities.js
@@ -0,0 +1,87 @@
+import { pool } from "../config/database.js";
+
+const createActivity = async (req, res) => {
+  const insertQuery = `
+    INSERT INTO activities (activity, trip_id)
+    VALUES($1, $2)
+    RETURNING *`;
+
+  try {
+    const trip_id = parseInt(req.params.trip_id);
+    const { activity } = req.body;
+
+    const results = await pool.query(insertQuery, [activity, trip_id]);
+    res.status(201).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const getActivities = async (req, res) => {
+  const selectQuery = `
+  SELECT *
+  FROM activities
+  ORDER BY id ASC`;
+
+  try {
+    const results = await pool.query(selectQuery);
+    res.status(200).json(results.rows);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const getTripActivities = async (req, res) => {
+  const selectQuery = `SELECT * FROM activities WHERE trip_id = $1`;
+
+  try {
+    const trip_id = parseInt(req.params.trip_id);
+    const results = await pool.query(selectQuery, [trip_id]);
+    res.status(200).json(results.rows);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const updateActivityLikes = async (req, res) => {
+  const updateLikeQuery = `
+    UPDATE activities
+    SET num_votes = $1
+    WHERE id = $2
+  `;
+
+  const { num_votes } = req.body;
+
+  try {
+    const id = parseInt(req.params.id);
+    const newVotes = parseInt(num_votes);
+
+    const results = await pool.query(updateLikeQuery, [newVotes, id]);
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const deleteActivity = async (req, res) => {
+  const deleteQuery = `
+    DELETE FROM activities
+    WHERE id = $1
+  `;
+
+  try {
+    const id = parseInt(req.params.id);
+    const results = await pool.query(deleteQuery, [id]);
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+export default {
+  getActivities,
+  getTripActivities,
+  createActivity,
+  deleteActivity,
+  updateActivityLikes
+};
